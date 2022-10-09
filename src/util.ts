ab1921ec9c65812f2d443ffa0b59289ad44daf2a
Anthony Campolo <admin@ajcwebdev.com>
2022-10-08 23:48:12 -0500

    refactor project structure

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,39 @@
-import type { MDXInstance, Post } from "./types";
+import type { MDXInstance, Page } from "astro";
+
+type Theme = "light" | "dark";
+
+interface IElement {
+	readonly as?: keyof HTMLElementTagNameMap;
+}
+
+type SiteMeta = {
+	title: string;
+	description?: string;
+	image?: string;
+};
+
+type PaginationLink = {
+	url: string;
+	text?: string;
+	srLabel?: string;
+};
+
+interface Post {
+	title: string;
+	description: string;
+	publishDate?: Date;
+	tags?: string[];
+}
+
+export type {
+	MDXInstance,
+	Page,
+	Theme,
+	IElement,
+	SiteMeta,
+	PaginationLink,
+	Post,
+};
 
 export function sortMDByDate(posts: MDXInstance<Post>[] = []) {
 	return posts.sort(
