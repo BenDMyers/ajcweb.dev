1d01788982c7ebcbb5e5b4a28a4638792d4bd405
Anthony Campolo <12433465+ajcwebdev@users.noreply.github.com>
2023-05-19 18:41:24 -0600

    prefetch, never again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
 import sitemap from "@astrojs/sitemap"
-import prefetch from "@astrojs/prefetch"
 
 export default defineConfig({
   site: "https://ajcwebdev.com/",
@@ -17,8 +16,7 @@ export default defineConfig({
 		mdx({}),
 		tailwind({ config: { applyBaseStyles: false } }),
     sitemap(),
-		image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-    prefetch()
+		image({ serviceEntryPoint: "@astrojs/image/sharp" })
   ],
   vite: {
 		optimizeDeps: {
